7c672dcbb9573ef705f5598a490eb78c638137b7
Doron Chen <cdoron@il.ibm.com>
2021-09-19 17:14:46 +0300

    parametrize download_example.sh
    Signed-off-by: Doron Chen <cdoron@il.ibm.com>

--- download_example.sh
+++ download_example.sh
@@ -1,2 +1,8 @@
-#RUST_LOG=oci_distribution=trace cargo run --example download-image -- webassembly.azurecr.io/hello-wasm
-RUST_LOG=oci_distribution=trace cargo run --example download-image -- the-mesh-for-data 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 ghcr.io/the-mesh-for-data/krustlet-tutorial:v1.0.0
+#!/bin/bash
+
+if [ $# -ne 3 ];
+    then echo "Usage: "$0" [username] [password] [wasm-oci-image]"
+    exit
+fi
+
+RUST_LOG=oci_distribution=trace cargo run --example download-image -- $1 $2 $3
